f8b011380ffeb54054e2d6e5e9e170a4ef98f81e
Kilderson Sena <dersonsena@gmail.com>
2021-02-09 11:13:48 -0300

    Adding entity and dto base classes into project

---
+++ src/Shared/Domain/ValueObject/Email.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Domain\ValueObject;
+
+use InvalidArgumentException;
+use JsonSerializable;
+
+final class Email implements JsonSerializable
+{
+    private string $email;
+
+    public function __construct(string $email)
+    {
+        if (empty($email)) {
+            throw new InvalidArgumentException('Email is empty.');
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException('Invalid email: ' . $email);
+        }
+
+        $this->email = trim($email);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param Email $email
+     * @return bool
+     */
+    public function isEqualsTo(Email $email): bool
+    {
+        return $this->email === (string)$email;
+    }
+
+    public function jsonSerialize(): string
+    {
+        return $this->__toString();
+    }
+}
