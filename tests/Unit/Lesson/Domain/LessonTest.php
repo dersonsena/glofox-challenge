eb821e79f4c197a546acea3f9faa226b1835f8f9
Kilderson Sena <dersonsena@gmail.com>
2021-02-11 19:19:22 -0300

    unit test to Lesson's dateIsWithinRange method

---
+++ tests/Unit/Lesson/Domain/LessonTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit\Lesson\Domain;
+
+use App\Lesson\Domain\Lesson;
+use App\Shared\Domain\ValueObjects\DateRange;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+
+class LessonTest extends TestCase
+{
+    private function getDateRangeMock()
+    {
+        $dateRangeMock = $this->getMockBuilder(DateRange::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $dateRangeMock->method('getStartDate')
+            ->willReturn(new DateTimeImmutable('2021-01-10'));
+
+        $dateRangeMock->method('getEndDate')
+            ->willReturn(new DateTimeImmutable('2021-01-15'));
+
+        return $dateRangeMock;
+    }
+
+    public function testIfDateIsWithinOfRange()
+    {
+        $lesson = new Lesson('any-name', $this->getDateRangeMock(), 1);
+
+        $this->assertTrue($lesson->dateIsWithinRange(new DateTimeImmutable('2021-01-10')));
+        $this->assertTrue($lesson->dateIsWithinRange(new DateTimeImmutable('2021-01-11')));
+        $this->assertTrue($lesson->dateIsWithinRange(new DateTimeImmutable('2021-01-12')));
+        $this->assertTrue($lesson->dateIsWithinRange(new DateTimeImmutable('2021-01-13')));
+        $this->assertTrue($lesson->dateIsWithinRange(new DateTimeImmutable('2021-01-14')));
+        $this->assertTrue($lesson->dateIsWithinRange(new DateTimeImmutable('2021-01-15')));
+    }
+
+    public function testIfDateIsOutOfRange()
+    {
+        $lesson = new Lesson('any-name', $this->getDateRangeMock(), 1);
+
+        $this->assertFalse($lesson->dateIsWithinRange(new DateTimeImmutable('2021-01-08')));
+        $this->assertFalse($lesson->dateIsWithinRange(new DateTimeImmutable('2021-01-09')));
+        $this->assertFalse($lesson->dateIsWithinRange(new DateTimeImmutable('2021-01-16')));
+        $this->assertFalse($lesson->dateIsWithinRange(new DateTimeImmutable('2021-01-17')));
+        $this->assertFalse($lesson->dateIsWithinRange(new DateTimeImmutable('2021-05-01')));
+        $this->assertFalse($lesson->dateIsWithinRange(new DateTimeImmutable('2021-06-22')));
+    }
+}
